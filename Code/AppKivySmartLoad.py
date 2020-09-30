86f7c6d1e9c4bfd68e30f4b2c44c7cacc0068a98
tec-zoop-pedro.sevenini <pedro.sevenini@zoop.com.br>
2020-09-30 19:52:24 -0300

    Adicionando Kivy e primeiro dsign

---
+++ Code/AppKivySmartLoad.py
@@ -0,0 +1,69 @@
+import kivy
+from kivy.app import App
+from kivy.uix.label import Label
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.textinput import TextInput
+from kivy.uix.button import Button
+
+
+class MyGrid(GridLayout):
+    def __init__(self, **kwargs):
+        super(MyGrid, self).__init__(**kwargs)
+        self.cols = 1
+
+        self.inside1 = GridLayout()
+        self.inside1.cols = 1
+
+        self.inside1.add_widget(Label(text="App Smart Load", font_size=40))
+
+        self.add_widget(self.inside1)
+
+
+        self.inside2 = GridLayout()
+        self.inside2.cols = 2
+
+        self.inside2.add_widget(Label(text="Load [Kg]: ", font_size=20))
+        self.load = TextInput(multiline=False, font_size=20)
+        self.inside2.add_widget(self.load)
+
+        self.inside2.add_widget(Label(text="Name: ", font_size=20))
+        self.name = TextInput(multiline=False, font_size=20)
+        self.inside2.add_widget(self.name)
+
+        self.inside2.add_widget(Label(text="Email: ", font_size=20))
+        self.email = TextInput(multiline=False,font_size=20)
+        self.inside2.add_widget(self.email)
+
+        self.add_widget(self.inside2)
+
+        self.inside3 = GridLayout()
+        self.inside3.cols = 2
+
+        self.inside3.submitCalibrate = Button(text="Calibrate", font_size=40)
+        self.inside3.submitCalibrate.bind(on_press=self.pressed)
+        self.inside3.add_widget(self.inside3.submitCalibrate)
+
+        
+        self.inside3.submitStart = Button(text="Start", font_size=40)
+        self.inside3.submitStart.bind(on_press=self.pressed)
+        self.inside3.add_widget(self.inside3.submitStart)
+
+        self.add_widget(self.inside3)
+
+    def pressed(self, instance):
+        name = self.name.text
+        load = self.load.text
+        email = self.email.text
+
+        print("Load:", load, "Name:", name, "Email:", email)
+        self.name.text = ""
+        self.load.text = ""
+        self.email.text = ""
+
+class MyApp(App):
+    def build(self):
+        return MyGrid()
+
+
+if __name__ == "__main__":
+    MyApp().run()
--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # AppSmartLoad
 
-Aplicativo em Python 
+Aplicativo em Python AppSmartLoad
 
 # Requisitos
 
@@ -23,14 +23,25 @@ $ sudo apt-get install python3-pip
 $ pip3 install numpy
 $ pip3 install matplotlib
 
+Para instalar as dependencias do Kivy siga o tutorial oficial: https://kivy.org/#download:
+$ python3 -m pip install --upgrade --user pip setuptools virtualenv
+$ python3 -m virtualenv ~/kivy_venv
+
+volta pra home
+$ python3 -m pip install kivy
+
+
 # Primeiros passos Windows
 Digite cmd na raiz do projeto para abri o terminal
 
 Para rodar o codigo basta executar o comando:
 $ python SmartLoad_11.py 
 
+Provavelmente você receberá erros pois precisamos instalar as dempendências:
 $ pip install numpy
 $ pip install matplotlib
+
+Para instalar as dependencias do Kivy siga o tutorial oficial: https://kivy.org/#download
 
 
 
