f53075061fe20e53857bd0f36de3981a1c3dcf76
tec-zoop-pedro.sevenini <pedro.sevenini@zoop.com.br>
2020-10-02 21:59:46 -0300

    Popup

--- Code/AppKivySmartLoad.py
+++ Code/AppKivySmartLoad.py
@@ -1,9 +1,30 @@
 from kivy.app import App
 from kivy.lang import Builder
+from kivy.properties import ObjectProperty
 from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.uix.popup import Popup
+from kivy.uix.label import Label
 
 class MainWindow(Screen):
-    pass
+    username = ObjectProperty(None)
+    email = ObjectProperty(None)
+    load = ObjectProperty(None)
+
+    def isValidLoad(self):
+        if (self.load.text != '' and float(self.load.text) > 0):
+            return True
+        else:  
+            self.invalidLoadPopup()
+            self.load.text = ''
+            return False
+
+    def invalidLoadPopup(self):
+
+            pop = Popup(title='Invalid Load',
+                  content=Label(text='The load must be a value greater than 0'),
+                  size_hint=(None, None), size=(350, 350) )
+            pop.open()
+    
 
 class CalibrateWindow(Screen):
     pass
@@ -13,12 +34,21 @@ class StartWindow(Screen):
 
 class WindowManager(ScreenManager):
     pass
+    
 
 kv = Builder.load_file("smartLoad.kv")
 
+screenmanager = WindowManager()
+
+screens = [MainWindow(name="main"), StartWindow(name="start"), CalibrateWindow(name="calibrate")]
+for screen in screens:
+    screenmanager.add_widget(screen)
+
+screenmanager.current = "main"
+
 class SmartLoadApp(App):
     def build(self):
-        return kv
+        return screenmanager
 
 if __name__ == "__main__":
     SmartLoadApp().run()
